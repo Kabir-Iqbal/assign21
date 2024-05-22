22abbbe9f044e0d4bb95b73e25cca524f892cfd0
Kabir-iqbal <161919126+Kabir-Iqbal@users.noreply.github.com>
2024-05-22 07:57:37 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// 21. They think of something you could store in a TypeScript Object. Write a program that creates 
+// Objects containing these items.
+let objectContaningitems = {
+    "laptop": {
+        name: "Lenvo laptop ",
+        price: 30000,
+        description: "High quaility in good condition "
+    },
+    "mouse": {
+        name: "mouse",
+        price: 700,
+        description: "Branded mouse "
+    },
+    "keyboard": {
+        name: "keyboard",
+        price: 1500,
+        description: "Stylish keyboard and good for speed work"
+    }
+};
+console.log(objectContaningitems);
+export {};
